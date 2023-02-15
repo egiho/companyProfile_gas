07d4c0c239af12ad721cb53b1178b0115cbb238a
egiho <dorkassinambela85@gmail.com>
2023-02-15 13:58:10 +0700

    perbaiki tampilan front_end

--- aplikasi_gas/resources/views/welcome.blade.php
+++ aplikasi_gas/resources/views/welcome.blade.php
@@ -202,7 +202,7 @@
                             <div class="middle_content_register">
                                 <div class="video_register video-container" data-aos="fade-right" data-aos-duration="1500">
                                     <iframe width="560" height="315" src="https://www.youtube.com/embed/VxVWACx9NIY" title="YouTube video player" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                                 
+
                                 </div>
                             </div>
                         </div>
